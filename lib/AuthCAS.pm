5a435bceedb84093495940665e2374428bc7d561
svendowideit <svendowideit@eccf29ea-9e2b-0410-8978-b52dbe5839ce>
2012-09-07 01:10:01 +0000

    add SSL_version option to constructor to allow for servers that cause problems      add pod skeletons for all functions
    git-svn-id: https://subversion.renater.fr/perlcas/trunk@49 eccf29ea-9e2b-0410-8978-b52dbe5839ce

--- lib/AuthCAS.pm
+++ lib/AuthCAS.pm
@@ -4,7 +4,7 @@ package AuthCAS;
 use strict;
 use vars qw( $VERSION);
 
-$VERSION = '1.5';
+$VERSION = '1.6';
 
 =head1 NAME
 
@@ -12,11 +12,13 @@ AuthCAS - Client library for CAS 2.0 authentication server
 
 =head1 VERSION
 
-Version 1.5
+Version 1.6
 
 =head1 DESCRIPTION
 
-AuthCAS aims at providing a Perl API to Yale's Central Authentication System (CAS). Only a basic Perl library is provided with CAS whereas AuthCAS is a full object-oriented library. 
+AuthCAS aims at providing a Perl API to Jasig Central Authentication System (CAS). 
+Only a basic Perl library is provided with CAS whereas AuthCAS is a full object-oriented library. 
+
 =head1 PREREQUISITES
 
 This script requires IO::Socket::SSL and LWP::UserAgent
@@ -77,7 +79,7 @@ Network
 
 =head1 DESCRIPTION
 
-CAS is Yale University's web authentication system, heavily inspired by Kerberos.
+Jasig CAS is Yale University's web authentication system, heavily inspired by Kerberos.
 Release 2.0 of CAS provides "proxied credential" feature that allows authentication
 tickets to be carried by intermediate applications (Portals for instance), they are
 called proxy.
@@ -86,12 +88,17 @@ This AuthCAS Perl module provides required subroutines to validate and retrieve 
 
 =head1 SEE ALSO
 
-Yale Central Authentication Service (http://www.yale.edu/tp/auth/) 
+Jasig Central Authentication Service (http://www.jasig.org/cas)
+
+was Yale Central Authentication Service (http://www.yale.edu/tp/auth/) 
+ 
  phpCAS (http://esup-phpcas.sourceforge.net/)
 
 =head1 COPYRIGHT
 
-Copyright (C) 2003 Comite Reseau des Universites (http://www.cru.fr). All rights reserved.
+Copyright (C) 2003, 2005,2006,2007,2009 Olivier Salaun - Comité Réseau des Universités (http://www.cru.fr)
+              2012 Sven Dowideit - SvenDowideit@fosiki.com
+
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
@@ -99,6 +106,7 @@ it under the same terms as Perl itself.
 =head1 AUTHORS
 
 Olivier Salaun
+Sven Dowideit
 
 =cut
 
@@ -109,33 +117,90 @@ my $errors;
 
 use Carp;
 
+=pod
+
+=head2 new
+
+  my $cas = new AuthCAS(
+		    casUrl => 'https://cas.myserver', 
+		    CAFile => '/etc/httpd/conf/ssl.crt/ca-bundle.crt',
+		    );
+
+The C<new> constructor lets you create a new B<AuthCAS> object.
+
+=over
+
+=item casUrl - REQUIRED
+=item CAFile
+=item CAPath
+=item loginPath - '/login'
+=item logoutPath - '/logout'
+=item serviceValidatePath - '/serviceValidate'
+=item proxyPath - '/proxy'
+=item proxyValidatePath - '/proxyValidate'
+=item SSL_version - unset
+Sets the version of the SSL protocol used to transmit data. If the default causes connection issues, setting it to 'SSLv3' may help.
+
+see the documentation for IO::Socket::SSL for more information
+
+see (http://www.perlmonks.org/?node_id=746493) for more details.
+
+=back
+
+Returns a new B<AuthCAS> or dies on error.
+
+=cut
+
 sub new {
     my ( $pkg, %param ) = @_;
-    my $cas_server = {};
+    my $cas_server = {
+        url    => $param{'casUrl'},
+        CAFile => $param{'CAFile'},
+        CAPath => $param{'CAPath'},
 
-    $cas_server->{'url'}    = $param{'casUrl'};
-    $cas_server->{'CAFile'} = $param{'CAFile'};
-    $cas_server->{'CAPath'} = $param{'CAPath'};
-
-    $cas_server->{'loginPath'}  = $param{'loginPath'}  || '/login';
-    $cas_server->{'logoutPath'} = $param{'logoutPath'} || '/logout';
-    $cas_server->{'serviceValidatePath'} = $param{'serviceValidatePath'}
-      || '/serviceValidate';
-    $cas_server->{'proxyPath'} = $param{'proxyPath'} || '/proxy';
-    $cas_server->{'proxyValidatePath'} = $param{'proxyValidatePath'}
-      || '/proxyValidate';
+        loginPath  => $param{'loginPath'}  || '/login',
+        logoutPath => $param{'logoutPath'} || '/logout',
+        serviceValidatePath => $param{'serviceValidatePath'}
+          || '/serviceValidate',
+        proxyPath         => $param{'proxyPath'}         || '/proxy',
+        proxyValidatePath => $param{'proxyValidatePath'} || '/proxyValidate',
+        SSL_version       => $param{SSL_version},
+    };
 
     bless $cas_server, $pkg;
 
     return $cas_server;
 }
 
-## Return module errors
+=pod
+
+=head2 get_errors
+
+Return module errors
+
+=cut
+
 sub get_errors {
     return $errors;
 }
 
-## Use the CAS object as a proxy
+=pod
+
+=head2 proxyMode
+
+Use the CAS object as a proxy
+
+
+=over
+
+=item pgtFile
+=item pgtCallbackUrl
+
+=back
+
+
+=cut
+
 sub proxyMode {
     my $self  = shift;
     my %param = @_;
@@ -162,6 +227,13 @@ sub _escape_chars {
 
     return $s;
 }
+
+=pod
+
+=head2 dump_var
+
+
+=cut
 
 sub dump_var {
     my ( $var, $level, $fd ) = @_;
@@ -245,6 +317,16 @@ sub _parse_xml {
     return \%xml_struct;
 }
 
+=pod
+
+=head2 getServerLoginURL($service)
+
+Returns a URL that you can redirect the browser to, which includes the URL to return to
+
+TODO: it escapes the return URL, but I've noticed some issues with more complicated URL's
+
+=cut
+
 sub getServerLoginURL {
     my $self    = shift;
     my $service = shift;
@@ -256,8 +338,15 @@ sub getServerLoginURL {
       . &_escape_chars($service);
 }
 
-## Returns non-blocking login URL
-## ie: if user is logged in, return the ticket, otherwise do not prompt for login
+=pod
+
+=head2 getServerLoginGatewayURL($service)
+
+Returns non-blocking login URL
+ie: if user is logged in, return the ticket, otherwise do not prompt for login
+
+=cut
+
 sub getServerLoginGatewayURL {
     my $self    = shift;
     my $service = shift;
@@ -270,8 +359,15 @@ sub getServerLoginGatewayURL {
       . '&gateway=1';
 }
 
-## Return logout URL
-## After logout user is redirected back to the application
+=pod
+
+=head2 getServerLogoutURL($service)
+
+Return logout URL
+After logout user is redirected back to the application
+
+=cut
+
 sub getServerLogoutURL {
     my $self    = shift;
     my $service = shift;
@@ -283,6 +379,14 @@ sub getServerLogoutURL {
       . &_escape_chars($service)
       . '&gateway=1';
 }
+
+=pod
+
+=head2 getServerServiceValidateURL($service, $ticket, $pgtUrl)
+
+Returns 
+
+=cut
 
 sub getServerServiceValidateURL {
     my $self    = shift;
@@ -303,6 +407,14 @@ sub getServerServiceValidateURL {
       . $query_string;
 }
 
+=pod
+
+=head2 getServerProxyURL($targetService, $pgt)
+
+Returns 
+
+=cut
+
 sub getServerProxyURL {
     my $self          = shift;
     my $targetService = shift;
@@ -315,6 +427,14 @@ sub getServerProxyURL {
       . &_escape_chars($targetService) . '&pgt='
       . &_escape_chars($pgt);
 }
+
+=pod
+
+=head2 getServerProxyValidateURL($service, $ticket)
+
+Returns 
+
+=cut
 
 sub getServerProxyValidateURL {
     my $self    = shift;
@@ -331,8 +451,18 @@ sub getServerProxyValidateURL {
 
 }
 
-## Validate a Service Ticket
-## Also used to get a PGT
+=pod
+
+=head2 validateST($service, $ticket)
+
+Validate a Service Ticket
+Also used to get a PGT
+
+
+Returns the login that created the ticket, if the ticket is valid for that $service URL
+
+=cut
+
 sub validateST {
     my $self    = shift;
     my $service = shift;
@@ -393,7 +523,16 @@ sub validateST {
     return ($user);
 }
 
-## Validate a Proxy Ticket
+=pod
+
+=head2 validatePT($service, $ticket)
+
+Validate a Proxy Ticket
+
+Returns the login that created the ticket, if the ticket is valid for that $service URL
+
+=cut
+
 sub validatePT {
     my $self    = shift;
     my $service = shift;
@@ -426,16 +565,30 @@ sub validatePT {
     return ( $user, @proxies );
 }
 
+=pod
+
+=head2 callCAS($url)
+
 ## Access a CAS URL and parses received XML
+
+Returns 
+
+=cut
+
 sub callCAS {
     my $self = shift;
     my $url  = shift;
 
     my ( $host, $port, $path ) = &_parse_url($url);
 
-    my @xml =
-      &get_https2( $host, $port, $path,
-        { 'cafile' => $self->{'CAFile'}, 'capath' => $self->{'CAPath'} } );
+    my @xml = &get_https2(
+        $host, $port, $path,
+        {
+            'cafile'      => $self->{'CAFile'},
+            'capath'      => $self->{'CAPath'},
+            'SSL_version' => $self->{'SSL_version'}
+        }
+    );
 
     unless (@xml) {
         warn $errors;
@@ -451,6 +604,12 @@ sub callCAS {
     return &_parse_xml( join( '', @xml ) );
 }
 
+=pod
+
+=head2 storePGT($pgtIou, $pgtId)
+
+=cut
+
 sub storePGT {
     my $self   = shift;
     my $pgtIou = shift;
@@ -465,6 +624,14 @@ sub storePGT {
 
     return 1;
 }
+
+=pod
+
+=head2 retrievePT($service)
+
+Returns 
+
+=cut
 
 sub retrievePT {
     my $self    = shift;
@@ -490,7 +657,18 @@ sub retrievePT {
     return undef;
 }
 
-# request a document using https, return status and content
+=pod
+
+=head2 get_https2
+
+request a document using https, return status and content
+
+Sven suspects this is intended to be private.
+
+Returns 
+
+=cut
+
 sub get_https2 {
     my $host = shift;
     my $port = shift;
@@ -539,6 +717,9 @@ sub get_https2 {
     ## If SSL_ca_file or SSL_ca_path => verify peer certificate
     $ssl_options{'SSL_verify_mode'} = 0x01
       if ( $trusted_ca_file || $trusted_ca_path );
+
+    $ssl_options{'SSL_version'} = $ssl_data->{'SSL_version'}
+      if defined( $ssl_data->{'SSL_version'} );
 
     $ssl_socket = new IO::Socket::SSL(%ssl_options);
 
